57a3282e350789d7bea2456d1dc5d678ce0f8421
Amit Singh <amit_nyc@live.com>
2018-07-13 17:47:25 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,6 +4,18 @@ class Appointment
 
 @@all = []
 
+def initialize(patient, doctor, date)
+  @patient = patient
+  @doctor = doctor
+  @date = date
+  @@all << self
+  patient.add_appointment(self)
 
+end
+
+
+def self.all
+  @@all
+end
 
 end
--- lib/patient.rb
+++ lib/patient.rb
@@ -4,7 +4,29 @@ class Patient
 
 def initialize(name)
   @name = name
-end 
+  @appointments = []
+end
+
+def appointments
+  @appointments
+end
+
+def add_appointment(appointment)
+  @appointments << appointment
+end
+
+
+# def new_appointment(doctor, date)
+#   appointment = Appointment.new(self, doctor, date)
+#   @appointments << appointment
+#   appointment
+# end
+
+def doctors
+ self.appointments.collect do |name|
+   name.appointments
+ end
+ end
 
 
 
